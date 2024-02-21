db7587f5d71b4fcc3f52f13d2036468459cbc261
cngwane <cngwane@CollinsBitsPC>
2024-02-21 00:43:15 -0500

    added arrowFunction Exercise

---
+++ arrowFunction/arraowFunction.js
@@ -0,0 +1,27 @@
+//In this exercise, you’ll refactor some ES5 code into ES2015.
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+
+//Refactor the above code to use two arrow functions. Turn it into a one-liner.
+const double = arr => arr.map(val => val * 2);
+
+//## **Refactor the following function to use arrow functions:**
+
+//Replace ALL functions with arrow functions:
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+  const squareAndFindEvens = number => number.map(num => num ** 2).filter(square => square % 2 === 0)
+
+
